37851b6676e399fab1544bfbdc6ea74f2314cb94
Sabinerobart <sabine.robart@protonmail.com>
2020-12-10 16:02:45 +0100

    feat: Add new 'Orders' route

--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getProducts, getIndex, getCart, getCheckout } = require('../controllers/shop');
+const { getProducts, getIndex, getCart, getCheckout, getOrders } = require('../controllers/shop');
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.get('/', getIndex);
 router.get('/products', getProducts);
 
 router.get('/cart', getCart);
+router.get('/orders', getOrders);
 router.get('/checkout', getCheckout);
 
 module.exports = router;
