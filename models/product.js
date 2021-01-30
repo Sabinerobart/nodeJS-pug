713ca3a1376e21e2b7a12706ba01466bebfd1315
Sabinerobart <sabine.robart@protonmail.com>
2021-01-30 14:03:28 +0100

    feat: Delete a product via mongoose

--- models/product.js
+++ models/product.js
@@ -21,30 +21,3 @@ const productSchema = new Schema({
 });
 
 module.exports = mongoose.model('Product', productSchema); // Set the schema's name
-
-
-
-// const getDb = require('../util/database').getDb;
-// const mongodb = require('mongodb');
-
-// class Product {
-//   constructor(title, price, description, imageUrl, id, userId) {
-//     this.title = title;
-//     this.price = price;
-//     this.description = description;
-//     this.imageUrl = imageUrl;
-//     this._id = id ? new mongodb.ObjectId(id) : null;
-//     this.userId = new mongodb.ObjectId(userId);
-//   }
-
-
-//   static deleteById(prodId) {
-//     const db = getDb();
-//     return db.collection('products')
-//       .deleteOne({ _id: new mongodb.ObjectId(prodId) })
-//       .then(() => console.log("Deleted !"))
-//       .catch(err => console.log("error in deleteById : ", err))
-//   }
-// }
-
-// module.exports = Product;
