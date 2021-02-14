4ea7f7bfebc475ded3f464463514c3b25ea91101
Sabinerobart <sabine.robart@protonmail.com>
2021-02-14 10:07:28 +0100

    feat: Add route protection

--- routes/shop.js
+++ routes/shop.js
@@ -1,25 +1,23 @@
-const path = require('path');
-
 const express = require('express');
-
-const shopController = require('../controllers/shop');
-
 const router = express.Router();
 
-router.get('/', shopController.getIndex);
+const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/isAuth');
 
-router.get('/products', shopController.getProducts);
+router.get('/', isAuth, shopController.getIndex);
 
-router.get('/products/:productId', shopController.getProduct);
+router.get('/products', isAuth, shopController.getProducts);
 
-router.get('/cart', shopController.getCart);
+router.get('/products/:productId', isAuth, shopController.getProduct);
 
-router.post('/cart', shopController.postCart);
+router.get('/cart', isAuth, shopController.getCart);
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post('/cart', isAuth, shopController.postCart);
 
-router.post('/create-order', shopController.postOrder);
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
-router.get('/orders', shopController.getOrders);
+router.post('/create-order', isAuth, shopController.postOrder);
+
+router.get('/orders', isAuth, shopController.getOrders);
 
 module.exports = router;
